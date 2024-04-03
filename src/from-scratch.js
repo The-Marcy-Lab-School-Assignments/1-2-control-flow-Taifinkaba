08d32f7194c3243c65802a95c92b3e2db4d75ef8
Taifinkaba <taifinkabad@gmail.com>
2024-04-03 16:44:57 -0400

    fixed some issues q1, q2

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,56 +1,54 @@
 const measureRain = (inches) => {
-  if(inches == 0){
-    return "drought";
-  }else if(inches > 0 && inches <= 2){
-    return "dry";
-  }else if(inches > 2 && inches <= 4){
-    return "average";
-  }else if(inches > 4 &&  inches <= 6){
-    return "rainy";
+  if(inches === 0){
+    console.log('drought');
+  }else if(inches < 2){
+    console.log("dry");
+  }else if(inches < 4){
+    console.log("average");
+  }else if(inches < 6){
+    console.log("rainy");
   }else{
-    return "flood";
+    console.log("flood");
   };
 };
-//console.log(measureRain(3));
+
 
 const happyBirthdayPet = (breed, age) => {
-  if(breed == 'cat' && age <  5){
-    return 'Mew  mew!';
+  if(breed === 'snake'){
+    console.log('Hiss hiss!');
+  }else if(breed == 'cat' && age <  5){
+    console.log('Mew mew!');
   }else if(breed == 'cat'  && age >= 5){
-    return 'Meow meow!';
+    console.log('Meow meow!');
   }else if(breed == 'dog'  && age < 5){
-    return 'Arf arf!';
-  }else if(breed == 'dog'  && age >= 5 || age < 10){
-    return 'Woof woof!';
+    console.log('Arf arf!');
+  }else if(breed == 'dog'  && age >= 5 && age < 10){
+    console.log('Woof woof!');
   }else if(breed == 'dog'  && age >= 10){
-    return 'Boof!';
-  }else if(breed  == 'snake'){
-    return 'Hiss hiss';
+    console.log('Boof!');
   }else{
-    return 'Happy  birthday!';
+    console.log('Happy birthday!');
   }
 };
 // console.log(happyBirthdayPet('cat', 6));
 
 const funTypes = (jsType) => {
   if(typeof jsType == 'string'){
-    return "That's just some text.";
+  console.log("That's just some text.");
   }else if(typeof jsType == 'number'){
-    return "That's a good number.";
+  console.log("That's a good number.");
   }else if(typeof jsType == 'boolean'){
-    return "To bool, or not to bool?";
+  console.log("To bool, or not to bool?");
   }else if(typeof jsType == 'undefined'){
-    return "Nothing, but I didn't set that.";
-  }else if(typeof jsType == 'undefined'){
-    return "Nothing, and I didn't set that.";
+  console.log("Nothing, but I didn't set that.");
   }else if(typeof jsType == 'null'){
-    return "Nothing, and I did set that.";
+  console.log("Nothing, and I did set that.");
   }else if(typeof jsType == 'object'){
-    return "Anybody got the key?";
+  console.log("Anybody got the key?");
   }else if(typeof jsType == 'array'){
-    return "I order you to be indexed.";
+  console.log("I order you to be indexed.");
   }else if(typeof jsType == 'NaN'){
-    return "Well, now you're just showing off.";
+  console.log("Well, now you're just showing off.");
   };
 };
 // console.log(funTypes('ying'))
