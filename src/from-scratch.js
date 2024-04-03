d6f258a50ee7f1233b55bf090f0598969f3e8709
Taifinkaba <taifinkabad@gmail.com>
2024-04-03 12:41:08 -0400

    question 1 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,6 +1,18 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if(inches == 0){
+    return "drought";
+  }else if(inches > 0 && inches <= 2){
+    return "dry";
+  }else if(inches > 2 && inches <= 4){
+    return "average";
+  }else if(inches > 4 &&  inches <= 6){
+    return "rainy";
+  }else{
+    return "flood";
+  };
 };
-
+//console.log(measureRain(3));
+/*
 const happyBirthdayPet = () => {
 };
 
@@ -12,11 +24,12 @@ const rounder = () => {
 
 const fizzBuzzish = () => {
 };
-
+*/
 module.exports = {
   measureRain,
-  happyBirthdayPet,
-  funTypes,
-  rounder,
-  fizzBuzzish,
+  // happyBirthdayPet,
+  // funTypes,
+  // rounder,
+  // fizzBuzzish,
 };
+
