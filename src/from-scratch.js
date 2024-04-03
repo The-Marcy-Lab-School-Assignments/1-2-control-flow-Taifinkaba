c4ba1e7c6365db6e4463ba4d511a33fe5ae301ac
Taifinkaba <taifinkabad@gmail.com>
2024-04-03 13:03:19 -0400

    question 2 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -12,13 +12,30 @@ const measureRain = (inches) => {
   };
 };
 //console.log(measureRain(3));
-/*
-const happyBirthdayPet = () => {
+
+const happyBirthdayPet = (breed, age) => {
+  if(breed == 'cat' && age <  5){
+    return 'Mew  mew!';
+  }else if(breed == 'cat'  && age >= 5){
+    return 'Meow meow!';
+  }else if(breed == 'dog'  && age < 5){
+    return 'Arf arf!';
+  }else if(breed == 'dog'  && age >= 5 || age < 10){
+    return 'Woof woof!';
+  }else if(breed == 'dog'  && age >= 10){
+    return 'Boof!';
+  }else if(breed  == 'snake'){
+    return 'Hiss hiss';
+  }else{
+    return 'Happy  birthday!';
+  }
 };
+// console.log(happyBirthdayPet('cat', 6));
 
 const funTypes = () => {
 };
 
+/*
 const rounder = () => {
 };
 
@@ -27,7 +44,7 @@ const fizzBuzzish = () => {
 */
 module.exports = {
   measureRain,
-  // happyBirthdayPet,
+  happyBirthdayPet,
   // funTypes,
   // rounder,
   // fizzBuzzish,
