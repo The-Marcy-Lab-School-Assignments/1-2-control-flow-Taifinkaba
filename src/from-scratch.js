9e3e81e23716fe19b1d70e06c076ea5348b6b941
Taifinkaba <taifinkabad@gmail.com>
2024-04-04 17:34:37 -0400

    question 1-5 work and are done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -33,50 +33,75 @@ const happyBirthdayPet = (breed, age) => {
 // console.log(happyBirthdayPet('cat', 6));
 
 const funTypes = (jsType) => {
-  if(typeof jsType == 'string'){
-  console.log("That's just some text.");
-  }else if(typeof jsType == 'number'){
-  console.log("That's a good number.");
-  }else if(typeof jsType == 'boolean'){
-  console.log("To bool, or not to bool?");
-  }else if(typeof jsType == 'undefined'){
-  console.log("Nothing, but I didn't set that.");
-  }else if(jsType === null){
-  console.log("Nothing, and I did set that.");
-  }else if(typeof jsType == 'object'){
-  console.log("Anybody got the key?");
-  }else if(typeof jsType == 'array'){
-  console.log("I order you to be indexed.");
-  }else if(typeof jsType == 'NaN'){
-  console.log("Well, now you're just showing off.");
-  };
-};
-// console.log(funTypes('ying'))
+  if (Number.isNaN(jsType)) {
+    console.log("Well, now you're just showing off.");
+  }
+  else if (Array.isArray(jsType)) {
+    console.log("I order you to be indexed.");
+  } 
+  else if (typeof jsType === 'string') {
+    console.log("That's just some text.");
+  }
+  else if (typeof jsType === 'undefined') {
+    console.log("Nothing, but I didn't set that.");
+  } 
+  else if (typeof jsType === 'number') {
+    console.log("That's a good number.");
+  } 
+  else if (typeof jsType === 'boolean') {
+    console.log("To bool, or not to bool?");
+  } 
+  else if (jsType === null) {
+    console.log("Nothing, and I did set that.");
+  } 
+  else if (typeof jsType === 'object') {
+    console.log("Anybody got the key?");
+  }
+}
+// funTypes('ying')
+//funTypes(  )
+// funTypes("string");
+// funTypes('');
+// funTypes(1);
+// funTypes(0);
+// funTypes(true);
+// funTypes(false);
+// funTypes(undefined);
+// funTypes(null);
+// funTypes({});
+// funTypes([]);
+// funTypes(NaN);
 
 
 const rounder = (float, roundingSetting) => {
-  if(roundingSetting === 'up' ) {
+  if(roundingSetting == 'up' ) {
     return Math.ceil(float);
-  }else if(roundingSetting === 'down'){
+  }else if(roundingSetting == 'down'){
     return Math.floor(float);
-  }else if(roundingSetting === 'honest'){
+  }else if(roundingSetting == 'honest'){
     return Math.round(float);
   };
 };
+// console.log(rounder(1.0, 'up'));
+// console.log(rounder(3.4, 'down'));
 // console.log(rounder(3.4, 'honest'));
 
 
 
 const fizzBuzzish = (num) => {
-  if(num % 3 == 0 && num % 5 == 0){
-    return "fizzBuzz!";
-  }else if(num % 3 == 0){
-    return "fizz";
-  }else if(num % 5 == 0){
-    return "buzz";
-  };
+  if(num % 3 === 0 && num % 5 === 0){
+    console.log("fizzBuzz!");
+  }else if(num % 3 === 0){
+    console.log("fizz");
+  }else if(num % 5 === 0){
+    console.log("buzz");
+  }else{
+    console.log(num);
+  }
 };
 // console.log(fizzBuzzish(15));
+
+
 
 module.exports = {
   measureRain,
